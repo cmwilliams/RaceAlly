94004d5839fe33d38d809d88914b9f8ea32cfadd
Mike Williams <cmwilliams@gmail.com>
2013-03-20 16:12:59 -0400

    Home Controller Modifications

--- RaceAlly.Web/Controllers/HomeController.cs
+++ RaceAlly.Web/Controllers/HomeController.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RaceAlly.Contracts.DataModel;
+using RaceAlly.Models;
 
 namespace RaceAlly.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IDataContext _context;
+
+        public HomeController(IDataContext context)
+        {
+            _context = context;
+        }
+
+
         public ActionResult Index()
         {
+            var user = _context.Users.Find(1);
+            var newRace = new Race { Name = "Test Race4", StartDate = DateTime.Now, User = user };
+            _context.Races.Add(newRace);
+            _context.SaveChanges();
+
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
